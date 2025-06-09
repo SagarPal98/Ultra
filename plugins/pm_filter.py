56fb6204fe86bd6e5511563b10c769d173568fd1
SagarPal98 <earningpoint973@gmail.com>
2025-06-09 20:48:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1276,12 +1276,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data == "start":
         buttons = [[
-                    InlineKeyboardButton('↔ ADD ME TO YOUR GROUP ↔', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('⇄ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ⇄', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🏷 𝚃𝚁𝙴𝙽𝙳𝙸𝙽𝙶', callback_data="topsearch"),
-                    InlineKeyboardButton('🎟 𝚄𝙿𝙶𝚁𝙰𝙳𝙴', callback_data="premium"),
+                    InlineKeyboardButton('🏷 ᴛʀᴇɴᴅɪɴɢ', callback_data="topsearch"),
+                    InlineKeyboardButton('🎟 ᴜᴘɢʀᴀᴅᴇ', callback_data="premium"),
                 ],[
-                    InlineKeyboardButton('💸 𝙴𝙰𝚁𝙽 𝚄𝙽𝙻𝙸𝙼𝙸𝚃𝙴𝙳 𝙼𝙾𝙽𝙴𝚈 𝚆𝙸𝚃𝙷 𝙱𝙾𝚃 💸', callback_data="earn")
+                    InlineKeyboardButton('💸 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 💸', callback_data="earn")
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await client.edit_message_media(
